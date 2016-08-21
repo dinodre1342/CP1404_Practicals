b401d2dd26ec7da787d622bd9d374aef6d8387ea
andresardinoarno <andresardino.arno@my.jcu.edu.au>
2016-08-21 17:58:25 +0800

    Assessment 1 Updates

--- assessment1/shoppinglist_main.py
+++ assessment1/shoppinglist_main.py
@@ -1,15 +1,32 @@
+"""
+Andre Sardino Arno
+
+The program is to load list of saved items from a csv file. The user then can choose either list required items, add new item,
+mark completed items, or list of completed items. The program ends with saving added new items to the csv file.
+"""
+
 def main():
     print("Shopping List 1.0 - by Andre Sardino Arno")
     print("3 items loaded from items.csv")
+    menu()
+
+
+
+def list_items ():
+    list = open("items.csv", 'r')
+    num = 0
+    for each in list:
+        items = each.strip().split()
+
+
+def menu():
     print("Menu:")
     print("R - List required items")
     print("A - Add new item")
     print("M - Mark an item as completed")
     print("Q - Quit")
-    user_input = input("Please select one:")
+    user_input = input(">>>")
 
-    while user_input == "":
-        user_input = input("Please select one:")
-
+    while True:
 
 main()
