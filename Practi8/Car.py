b9368cfab976705b9098d2eccd9f835d7ce444e5
andresardinoarno <andresardino.arno@my.jcu.edu.au>
2016-09-20 23:56:19 +0800

    Practi8 & 9

--- Practi8/Car.py
+++ Practi8/Car.py
@@ -1,14 +1,14 @@
- import random
- class Car:
+import random
+class Car:
     """ represent a car object """
 
-    def _init_(self, name="", fuel=0):
+    def __init__(self, name="", fuel=0):
         """ initialise a Car instance """
         self.name = name
         self.fuel = fuel
         self.odometer = 0
 
-    def _str_(self):
+    def __str__(self):
         return "{}, fuel={}, odo={}".format(self.name, self.fuel, self.odometer)
 
     def add_fuel(self, amount):
@@ -28,42 +28,31 @@
         return distance_driven
 
 
- class UnreliableCar(Car):
-     """ represent a car object """
+class UnreliableCar(Car):
+    """ represent a car object """
 
-     def _init_(self, fuel, reliability):
-         """ initialise a Car instance """
-         self.fuel = fuel
-         self.reliability = reliability
+    def __init__(self, fuel, reliability):
+        self.fuel = fuel
+        self.reliability = reliability
 
-     def _str_(self):
-         return "{}, fuel={}, odo={}".format(self.name, self.fuel, self.odometer)
+    def __str__(self):
+        return "{}, fuel={}, odo={}".format( self.fuel, self.odometer)
 
-     def drive(self, distance):
-         """ drive the car a given distance if it has enough fuel or drive until fuel runs out
-         return the distance actually driven """
-         print(self.reliability.random)
-
-         if distance > self.fuel:
-             distance_driven = self.fuel
-             self.fuel = 0
-         else:
-             self.fuel -= distance
-             distance_driven = distance
-         self.odometer += distance_driven
-         return distance_driven
+    def drive(self, distance):
+        if self.reliability > random.randint(0,100):
+            super().drive(distance)
 
 
 class Taxi(Car):
     """ specialised version of a Car that includes fare costs """
 
-    def _init_(self, name, fuel, price_per_km):
+    def __init__(self, name, fuel, price_per_km):
         """ initialise a Taxi instance, based on parent class Car """
         super().init_(name, fuel)
         self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
-    def _str_(self):
+    def __str__(self):
         """ return a string representation like a car but with current fare distance"""
         return "{}, ${:.2f}/km, {}km on current fare".format(super().str_(), self.price_per_km,
                                                              self.current_fare_distance)
@@ -81,6 +70,50 @@ class Taxi(Car):
         distance_driven = super().drive(distance)
         self.current_fare_distance += distance_driven
         return distance_driven
+
+    def get_current_fair_distance(self):
+        return self.current_fare_distance
+
+    def get_fuel(self):
+        return self.fuel
+
+    def set_current_fair_distance(self, distance):
+        self.current_fare_distance = distance
+
+    def set_price_per_km(self, scale):
+        self.price_per_km *= scale
+
+if __name__ == "main":
+    Prius1 = Taxi("Prius 1", 100)
+    Prius1.drive(40)
+    print("Fare: ${}\n".format(Prius1.get_fare()))
+
+    Prius1.set_current_fair_distance(0)
+    Prius1.drive(100)
+    print(Prius1)
+    print("Fare: ${}\n".format(Prius1.get_fare()))
+
+class SilverServiceTaxi(Car):
+    def __init__(self, name="", fuel=0, fanciness=50):
+        super().__init__(name, fuel)
+        super().set_price_per_km(fanciness)
+
+        self.new_fare = 0
+        self.flagfall = 4.5
+
+    def get_fare(self):
+        """Get the price of the taxi trip"""
+        if not self.new_fare:
+            return "{:.{prec}f}".format((self.price_per_km * self.current_fare_distance) + 4.5, prec=2)
+        else:
+            return "{:.{prec}f}".format(self.price_per_km * self.current_fare_distance, prec=2)
+
+newTaxi = SilverServiceTaxi("Silver Service", 100, 2)
+print(newTaxi)
+newTaxi.driver(10)
+print ("Fare: ${}".format(newTaxi.get_fare()))
+
+
 
 
 
